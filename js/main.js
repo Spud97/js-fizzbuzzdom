f791f5463a35197dcd70004d91f53d5223cd46a1
Spud97 <luca.dercole97@gmail.com>
2022-05-20 17:28:07 +0200

    ticket

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     console.log("Fizz");
   } else if (i % 5 === 0) {
@@ -9,3 +9,5 @@ for (let i = 0; i <= 100; i++) {
     console.log(i);
   }
 }
+
+const container = document.querySelector("[name='container']");
