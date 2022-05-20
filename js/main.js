0a4e47ab28070c44f7adba08f2783ca53e51d857
Spud97 <luca.dercole97@gmail.com>
2022-05-20 19:14:39 +0200

    Ultimo tentativo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
+const row = document.querySelector(".row");
+let printHtml;
+
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log("Fizz");
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    printHtml = "FizzBuzz";
+    classColor = "fizz-buzz-color";
   } else if (i % 5 === 0) {
     console.log("Buzz");
-  } else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+    printHtml = "Buzz";
+    classColor = "buzz-color";
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    printHtml = "Fizz";
+    classColor = "fizz-color";
   } else {
     console.log(i);
+    printHtml = i;
+    classColor = "standard-color";
   }
+  row.innerHTML += `<div class="col px-0">
+                      <div class="square ${classColor}">${printHtml}</div>
+                    </div>`;
 }
-
-const container = document.querySelector("[name='container']");
